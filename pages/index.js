2fe536353ec282ffe4dfda1e79c0a141c87d8d5f
Andres Oliveira <andresoliveira@protonmail.com>
2024-03-02 16:21:28 -0300

    Modal Add Folder

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,15 @@ import {
   Navbar,
   Button,
   Modal,
+  Form
 } from 'react-bootstrap';
 import { useState } from 'react';
 
 const Home = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <Container fluid>
@@ -25,6 +30,33 @@ const Home = () => {
           </Button>
         </Container>
       </Navbar>
+      { 
+        <>
+          {/* Modal */}
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                  {/* Folder Name */}
+                  <Form.Group className="mb-3" controlId="formFolderName">
+                      <Form.Label>Nome da Pasta:</Form.Label>
+                      <Form.Control type="text" placeholder="Ex: Fotos" />
+                  </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Fechar
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Criar
+                </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      }
     </Container>
   )
 }
