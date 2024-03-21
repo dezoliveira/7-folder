05a47e76415fc9de4e0fd6940e4c078a944f2036
Andres Oliveira <andresoliveira@protonmail.com>
2024-03-21 13:01:52 -0300

    Added build structure + next.config.js

---
+++ .next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"a785ea69f5bce7ef08a0fd36a5a52c68\",\"previewModeSigningKey\":\"5c2d9f60b096811c1d7c947ab95ef60eb783288de846c8ff71a9586c39d335d0\",\"previewModeEncryptionKey\":\"6f082d94432a9e8a5127fcd7f10a43954e1aa789b79a3317f7cc6ab7427b3619\"},\"notFoundRoutes\":[]}"
